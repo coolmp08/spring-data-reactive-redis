bbde05603502ea3205b38aec37e59addb3cd838b
coolmp08 <coolmp08@gmail.com>
2024-12-05 01:37:02 +0530

    initial commit

---
+++ src/main/java/io/devsense/spring_data_reactive_redis/SpringDataReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package io.devsense.spring_data_reactive_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataReactiveRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataReactiveRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devsense/spring_data_reactive_redis/SpringDataReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devsense.spring_data_reactive_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
